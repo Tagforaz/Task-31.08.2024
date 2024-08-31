febcca9339956aa1b8348b06b557858238b6054f
Tagforaz <nasibshahverdiyev@gmail.com>
2024-08-31 14:07:40 +0400

    Task done

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿#region Task4
+//string words = ("jsjs");
+//bool result = false;
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i] == 'a')
+//    {
+//        result = true;
+//        break;
+//    }
+//}
+//if (result)
+//{
+//    Console.WriteLine("a herfi var");
+//}
+//else
+//{
+//    Console.WriteLine("a herfi yoxdu");
+//}
+
+#endregion
+
+
+#region Task3
+
+//int[] nums = { 1, 3, 4, 8, 9, 33, 45, 56 };
+//int count = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] / 10 == 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+#endregion
+
+#region Task2
+
+//string[] words = { "azerbaycan", "uc", "masin" };
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i].Length > 4)
+//    {
+//        Console.WriteLine(words[i]);
+//    }
+//}
+
+#endregion
+
+#region Task1
+//int[] nums = { 8, 4, 7, 2, 15, 23 };
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    for (int j = 0; j < nums.Length - 1; j++)
+//    {
+//        if (nums[i] > nums[j + 1])
+//        {
+//            int temp = nums[j];
+//            nums[j] = nums[j + 1];
+//            nums[j + 1] = temp;
+//        }
+//    }
+//}
+
+//{
+//    Console.WriteLine(nums[0]);
+//}
+
+#endregion
